a780d42a368708934997658b5fe0fa7e82251eff
jiangquanxing <jiangquanxing@sinog2c.com>
2022-12-06 15:33:53 +0800

    fix: 框架整改

--- src/views/system/login/utils/rule.ts
+++ src/views/system/login/utils/rule.ts
@@ -1,5 +1,9 @@
 import { reactive } from "vue";
+import { isPhone } from "@pureadmin/utils";
 import type { FormRules } from "element-plus";
+
+/** 6位数字验证码正则 */
+export const REGEXP_SIX = /^\d{6}$/;
 
 /** 密码正则（密码格式应为8-18位数字、字母、符号的任意两种组合） */
 export const REGEXP_PWD =
@@ -11,18 +15,68 @@ const loginRules = reactive(<FormRules>{
   //   {
   //     validator: (rule, value, callback) => {
   //       if (value === "") {
-  //         callback(new Error("请输入密码"));
+  //         callback(new Error(transformI18n($t("login.passwordReg"))));
   //       } else if (!REGEXP_PWD.test(value)) {
-  //         callback(
-  //           new Error("密码格式应为8-18位数字、字母、符号的任意两种组合")
-  //         );
+  //         callback(new Error(transformI18n($t("login.passwordRuleReg"))));
   //       } else {
   //         callback();
   //       }
   //     },
   //     trigger: "blur"
   //   }
-  // ]
+  // ],
 });
 
-export { loginRules };
+/** 手机登录校验 */
+const phoneRules = reactive(<FormRules>{
+  phone: [
+    {
+      validator: (rule, value, callback) => {
+        if (value === "") {
+          callback(new Error("请输入手机号码"));
+        } else if (!isPhone(value)) {
+          callback(new Error("请输入正确的手机号码格式"));
+        } else {
+          callback();
+        }
+      },
+      trigger: "blur"
+    }
+  ]
+});
+
+/** 忘记密码校验 */
+const updateRules = reactive(<FormRules>{
+  phone: [
+    {
+      validator: (rule, value, callback) => {
+        if (value === "") {
+          callback(new Error("请输入手机号码"));
+        } else if (!isPhone(value)) {
+          callback(new Error("请输入正确的手机号码格式"));
+        } else {
+          callback();
+        }
+      },
+      trigger: "blur"
+    }
+  ],
+  password: [
+    {
+      validator: (rule, value, callback) => {
+        if (value === "") {
+          callback(new Error("请输入密码"));
+        } else if (!REGEXP_PWD.test(value)) {
+          callback(
+            new Error("密码格式应为8-18位数字、字母、符号的任意两种组合")
+          );
+        } else {
+          callback();
+        }
+      },
+      trigger: "blur"
+    }
+  ]
+});
+
+export { loginRules, phoneRules, updateRules };
