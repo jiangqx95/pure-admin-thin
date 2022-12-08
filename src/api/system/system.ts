4e20d8361318e244f6d9e3663eb354cdf318f0dd
jiangqx <740570905@qq.com>
2022-12-08 22:47:40 +0800

    feat: 框架整改

--- src/api/system/system.ts
+++ src/api/system/system.ts
@@ -3,15 +3,21 @@ import { baseApi } from "@/api/utils";
 
 /** 获取用户管理列表 */
 export const getUserList = (params?: object) => {
-  return http.request<Result<Page>>("get", baseApi("api/users"), { params });
+  return http.request<BaseApiResult<Page>>("get", baseApi("api/users"), {
+    params
+  });
 };
 
 /** 获取角色管理列表 */
 export const getRoleList = (params?: object) => {
-  return http.request<Result<Page>>("get", baseApi("api/roles"), { params });
+  return http.request<BaseApiResult<Page>>("get", baseApi("api/roles"), {
+    params
+  });
 };
 
 /** 获取部门管理列表 */
 export const getDeptList = (params?: object) => {
-  return http.request<Result<Page>>("get", baseApi("api/dept"), { params });
+  return http.request<BaseApiResult<Page>>("get", baseApi("api/dept"), {
+    params
+  });
 };
